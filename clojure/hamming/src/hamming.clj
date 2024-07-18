1c0a762645f69cb2eefb5b40334db6dbd756d124
Matthew Darling <matthewjdarling@gmail.com>
2024-07-18 00:00:00 -0400

    Add solution to hamming

---
+++ clojure/hamming/src/hamming.clj
@@ -0,0 +1,9 @@
+(ns hamming)
+
+(defn distance
+  [strand1 strand2]
+  (when (= (count strand1) (count strand2))
+    (reduce +
+            (map #(if (= %1 %2) 0 1)
+                   strand1
+                   strand2))))
