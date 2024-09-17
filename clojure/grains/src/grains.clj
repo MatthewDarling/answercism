c329f500f85f0eb03a691b9edf8719a70007f17c
Matthew Darling <matthewjdarling@gmail.com>
2024-09-17 15:16:33 -0400

    Comment out the broken bit shift function

--- clojure/grains/src/grains.clj
+++ clojure/grains/src/grains.clj
@@ -1,13 +1,14 @@
 (ns grains)
 
-(defn square-broken
-  "Clojure's BigInt class doesn't support bit shifts"
-  [square-num]
-  (bit-shift-left (bigint 1) (dec square-num)))
+(comment
+  (defn square-broken
+    "Clojure's BigInt class doesn't support bit shifts, this throws an exception"
+    [square-num]
+    (bit-shift-left (bigint 1) (dec square-num))))
 
 (defn square
   "Clojure's bit-shift-left helper also falsely claims the Java BigInteger class
-  doesn't support bit shifts"
+  doesn't support bit shifts, so we have to use the Java method"
   [square-num]
   (.shiftLeft (biginteger 1) (dec square-num)))
 
