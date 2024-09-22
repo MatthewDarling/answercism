7ca7da922d9ecbd0c75d3a16dc474186fac38fc2
Matthew Darling <matthewjdarling@gmail.com>
2024-09-21 23:44:26 -0400

    Rewrite Clj all-your-base with same algo as Python
    The positional notation stuff made this way more complicated than it
    needed to be. It was easy enough to convert the input digits into base10
    with `map-indexed`, but trying to invert that operation was gnarly.
    Because the power you raise each digit on depends on the number of
    digits, I had `biggest-power-of-base` to figure out the number of
    digits, then the `loop`+`recur` to go through each step.
    
    But it turns out there's a much simpler way! Rather than mess with any
    of exponents, just repeatedly multiply or divide by the base. Which is
    equivalent but far simpler for a programmatic algorithm.

--- clojure/all-your-base/src/all_your_base.clj
+++ clojure/all-your-base/src/all_your_base.clj
@@ -2,44 +2,47 @@
 
 (defn valid-request?
   [input-base num-seq target-base]
-  (and (every? (complement neg?) num-seq)
-       (every? #(< % input-base) num-seq)
+  (and (every? (complement neg?) num-seq) ;; Positive digits only
+       (every? #(< % input-base) num-seq) ;; Digits can't be larger than base
+       ;; No unary numeral system support
        (>= input-base 2)
        (>= target-base 2)))
 
-(defn biggest-power-of-base
-  ([decimal-num target-base]
-   (biggest-power-of-base decimal-num target-base 0))
-  ([decimal-num target-base current-pow]
-   (let [next-pow (inc current-pow)]
-     (if (< decimal-num (Math/pow target-base next-pow))
-       current-pow
-       (recur decimal-num target-base next-pow)))))
-
-(defn decimal-to-base
-  [decimal-num target-base]
-  (loop [output []
-         current-pow (biggest-power-of-base decimal-num target-base)
-         remaining decimal-num]
-    (if (neg? current-pow)
-      output
-      (let [;; Math/pow returns doubles, we're just dealing with ints
-            divisor (int (Math/pow target-base current-pow))
-            ;; casting to int rounds down
-            quotient (int (/ remaining divisor))]
-        (recur (conj output quotient)
-               (dec current-pow)
-               (- remaining (* quotient divisor)))))))
+(defn reduce-helper
+  "Rather than do a `loop`+`recur`, I thought this would read fairly nicely as a
+  `reduce`, using `reduced` to end when we hit the base case. Except the base
+  case is a separate variable from the output, so we accumulate with a map. And
+  we don't have a sequence to iterate over, but we _do_ have another variable we
+  need for our division and modulus. So we `reduce` across an infinite sequence
+  of `target-base`. It's a bit too clever, but it's easy to follow with an
+  explanation."
+  [{:keys [output remaining]} target-base]
+  (if (zero? remaining)
+    (reduced output)
+    {:output (conj output (mod remaining target-base))
+     :remaining (int (/ remaining target-base))}))
 
 (defn convert
   [input-base num-seq target-base]
   (when (valid-request? input-base num-seq target-base)
-    (let [num-seq-as-base-10 (->> num-seq
-                                  reverse
-                                  (map-indexed #(* %2 (Math/pow input-base %1))))]
-      (if (seq num-seq-as-base-10)
-        (-> num-seq-as-base-10
-            (->> (reduce +))
-            long
-            (decimal-to-base target-base))
-        num-seq-as-base-10))))
+    ;; Exercism recommends "Positional Notation" where you multiply the digits
+    ;; by successive powers of input-base. However, this can be factored out
+    ;; into addition of the digits and repeated multiplications of the base
+    ;; Taken from the Python community solutions, thank you!
+    (let [as-base-10 (reduce #(-> %1 (* input-base) (+ %2))
+                             0
+                             num-seq)]
+      (if (zero? as-base-10)
+        (if (seq num-seq) [0] [])
+        ;; Iterate over an infinite sequence of `target-base`, with repeated
+        ;; modulus and division operations to convert from base10
+        ;;
+        ;; Using a list as the accumulator because each successive modulus
+        ;; represents a bigger piece of the value, so we want to add at the
+        ;; front
+        (->> target-base
+             repeat
+             (reduce reduce-helper
+                     {:output '()
+                      :remaining as-base-10})
+             seq)))))
