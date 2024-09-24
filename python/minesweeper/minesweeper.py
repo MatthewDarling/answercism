3d0033450aae8b617f65bb76b9c01c3b3b366921
Matthew Darling <matthewjdarling@gmail.com>
2024-09-23 22:32:43 -0400

    Move more logic into nested loop in Minesweper
    I like throwing the exception while iterating, we already have to check
    every character so the `all` was extra work.
    
    In the deep dive video on the problem, a few solutions had a structure
    like:
    
    [[-1, -1], [-1, 0], [1, 1]]
    
    etc for the eight adjacent tiles. Which is what I was doing with the big
    pile of zips and ranges, but clearer. I swapped that for a nested
    list expression, then a check to ignore the (0, 0) case.
    
    The `mine_neighbours or " "` bit is much nicer than the ternary I had
    previously, as well.
    
    Only thing I find a bit worse about this one is the giant if that
    simulatenously checks boundaries, ignores the chosen tile, and then
    finally checks for a mine. It would probably be better to split it up,
    but I'm gonna leave it for now and work on other problems first.

--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -1,18 +1,21 @@
 def annotate(minefield):
     if any(len(x) != len(minefield[0]) for x in minefield):
         raise ValueError("The board is invalid with current input.")
-    if minefield and minefield[0] and not all(y == "*" or y == " " for x in minefield for y in x):
-        raise ValueError("The board is invalid with current input.")
 
     for column_index, row in enumerate(minefield):
         row_list = list(row)
         for row_index, item in enumerate(row):
             if item == "*":
                 continue
-            squares_to_check = list(zip(range(column_index-1, column_index+2), [row_index-1]*3)) + list(zip([column_index-1, column_index+1], [row_index]*2)) + list(zip(range(column_index-1, column_index+2), [row_index+1]*3))
-            squares_to_check = [x for x in squares_to_check if x[0] >= 0 and x[1] >= 0 and x[0] < len(minefield) and x[1] < len(row_list)]
-            adjacent_mines = len([x for x in squares_to_check if minefield[x[0]][x[1]] == "*"])
-            row_list[row_index] = " " if not adjacent_mines else str(adjacent_mines)
+            if item != " ":
+                raise ValueError("The board is invalid with current input.")
+
+            mine_neighbours = 0
+            for neighbour in [(x,y) for x in range(column_index-1, column_index+2) for y in range(row_index-1, row_index+2)]:
+                if neighbour[0] >= 0 and neighbour[0] < len(minefield) and neighbour[1] >= 0 and neighbour[1] < len(row_list) and neighbour != (column_index, row_index) and minefield[neighbour[0]][neighbour[1]] == "*":
+                    mine_neighbours += 1
+
+            row_list[row_index] = str(mine_neighbours or " ")
         minefield[column_index] = "".join(row_list)
     
     return minefield
